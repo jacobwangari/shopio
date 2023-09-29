5df4117cbd0bf956d3ac61ffd242e0232fc8f203
jacobwangari <mwasjac@gmail.com>
2023-09-29 07:32:17 +0300

    products fields changed to float

--- base/models.py
+++ base/models.py
@@ -13,8 +13,8 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     stock = models.IntegerField()
-    purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
-    selling_price = models.DecimalField(max_digits=10, decimal_places=2)
+    purchase_price = models.FloatField()
+    selling_price = models.FloatField()
 
     def __str__(self):
         return self.name
